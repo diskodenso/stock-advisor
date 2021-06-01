795005a6e0118c5a17b6c0cfeaeb93b1f234fc9a
Denis Olf <denisadnanolf@gmail.com>
2021-06-01 10:22:16 +0200

    firebase,  registration page, .env  and context

---
+++ src/Views/Login.js
@@ -0,0 +1,10 @@
+import React { useState, useContext } from 'react'
+import AuthContext from "../Context/authContext.js";
+
+export default function Login() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+var firebaseConfig = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: "stock-advisor-c63af.firebaseapp.com",
+    projectId: "stock-advisor-c63af",
+    storageBucket: "stock-advisor-c63af.appspot.com",
+    messagingSenderId: "456188025138",
+    appId: "1:456188025138:web:0be9d9d2d548d70ac5df2b"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  
+export default firebase;
+// do it with all vars
