7d5f5d29614f903136e9c1a625cc1496ef12c0a9
Denis Olf <denisadnanolf@gmail.com>
2021-06-02 09:26:36 +0200

    Login Page, Firebase, React

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,10 +1,71 @@
-import React { useState, useContext } from 'react'
-import AuthContext from "../Context/authContext.js";
+import React, { useState, useContext } from "react";
+import { AuthContext } from "../Context/authContext";
+import firebase from "../firebaseConfig.js";
 
-export default function Login() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+// function component ( with capital letters) - holding the email and passwort 
+const Login = () => {
+  const [state, setState] = useState({ email: "", password: "" });
+  const { user, setUser, isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+// {} = object - watch javascript basics spike 
+// [] = array
+// () = not a javascript type - define a function
+    
+// normal login function
+  const login = () => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(state.email, state.password)
+      .then((userCredential) => {
+        // Signed in
+          const user = userCredential.user;
+          console.log(userCredential);
+        console.log(`user in login`, user);
+        setUser(user);
+        setIsLoggedIn(true);
+        // ...
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        setUser(null); // falsy value 
+        setIsLoggedIn(false);
+      });
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    login();
+  };
+
+  return (
+    <form onSubmit={handleOnSubmit}>
+      <label>
+        <p>email</p>
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={state.email}
+        />
+      </label>
+      <label>
+        <p>Password</p>
+        <input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          value={state.password}
+        />
+      </label>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default Login;
